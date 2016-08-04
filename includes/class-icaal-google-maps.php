e37f5bde139c95bd22fee6117048193db953c901
ashdotguru <hello@ash.guru>
2016-08-04 11:59:36 +0100

    register post type

--- includes/class-icaal-google-maps.php
+++ includes/class-icaal-google-maps.php
@@ -153,6 +153,7 @@ class Icaal_Google_Maps {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_admin, 'register_post_type' );
 
 	}
 
